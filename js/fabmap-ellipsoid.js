cb6b5c4700af45a6c95ae888e8133cfec2ecbd9a
essitam <matisseam@hotmail.com>
2020-03-28 14:32:16 -0400

    eep

--- js/fabmap-ellipsoid.js
+++ js/fabmap-ellipsoid.js
@@ -31,13 +31,14 @@ function preload() {
       mimoAmsterdam = ["mimoTraces/	003.jpg	", "mimoTraces/	004.jpg	", "mimoTraces/	005.jpg	", "mimoTraces/	025.png	", "mimoTraces/	026.png	", "mimoTraces/	027.png	", "mimoTraces/	028.png	", "mimoTraces/	029.png	", "mimoTraces/	030.png	", "mimoTraces/	031.png	", "mimoTraces/	032.png	", "mimoTraces/	033.png	", "mimoTraces/	034.png	", "mimoTraces/	035.png	", "mimoTraces/	036.png	", "mimoTraces/	037.png	", "mimoTraces/	038.png	", "mimoTraces/	102.jpg	", "mimoTraces/	103.jpg	", "mimoTraces/	104.jpg	", "mimoTraces/	105.jpg	", "mimoTraces/	106.jpg	", "mimoTraces/	107.jpg	", "mimoTraces/	108.jpg	", "mimoTraces/	109.jpg	", "mimoTraces/	110.jpg	", "mimoTraces/	121.jpg	", "mimoTraces/	122.jpg	", "mimoTraces/	123.jpg	", "mimoTraces/	124.jpg	", "mimoTraces/	125.jpg	", "mimoTraces/	126.jpg	", "mimoTraces/	127.jpg	", "mimoTraces/	128.jpg	", "mimoTraces/	212.jpg	", "mimoTraces/	213.jpg	", "mimoTraces/	214.jpg	", "mimoTraces/	215.jpg	", "mimoTraces/	230.jpg	", "mimoTraces/	231.jpg	", "mimoTraces/	232.jpg	", "mimoTraces/	233.jpg	", "mimoTraces/	235.jpg	", "mimoTraces/	236.jpg	", "mimoTraces/	325.jpg	"];
       pictures = ['thingies/beige.png', 'thingies/pinkpatch.png', 'thingies/plantplastic.png', 'thingies/polkaslink.png', 'thingies/posterpatch.png', 'thingies/posters.png', 'thingies/prepillowcreature.png'];
       // train = loadModel('models/agrisphere-stones-obj/agrisphere-stones.obj');
-      rand = loadImage(random(pictures));
-      amstrand = loadImage(random(mimoAmsterdam));
+      // rand = loadImage(random(pictures));
+      rand = loadImage(random(mimoAmsterdam));
 }
 
 function setup() {
   fabb = createCanvas(windowWidth, windowHeight, WEBGL);
   fabb.parent('fabmap');
+  fabb.mousePressed(pauseCanvas);
   // mimoText = createGraphics(2000, 500);
   // mimoText.text(mimo[0], mimo[0].length, mimo[0].length);
 
@@ -125,7 +126,7 @@ function zoomIn() {
   console.log(cameraZ);
 }
 
-function mousePressed() {
+function pauseCanvas() {
   running = !running // flip the boolean
 }
 function svvi(x){
